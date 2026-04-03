2b4f211bb8a60404601795edb1a7b05a88f3c5b3
James <jtclarkjr@ucla.edu>
2026-04-03 22:46:23 +0900

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,8 @@ import type { StorybookConfig } from '@storybook/nextjs-vite'
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@chromatic-com/storybook',
+    // Work around duplicate preview annotation registration in Storybook 10.3.
+    '@chromatic-com/storybook/preset',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
